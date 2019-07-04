abbdf1ddc88e252b8bc75b340da32ec302d14f50
FukeKazki <kazkichi0906@gmail.com>
2019-07-04 17:33:30 +0900

    Update: server.jsの基本設定

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-export default config = {
-    channelAccessToken: '',
-    channelSecret: ''
-};
+import express from 'express';
+import * as line from '@line/bot-sdk';
+import crypto from 'crypto';
+import axios from 'axios';
+import config  from './setting';
+
+const app = express();
+const client = new line.Client(config);
+const PORT = process.env.PORT || 3000;
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+
+});
