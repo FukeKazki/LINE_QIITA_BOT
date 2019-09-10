3d833a57c5f1a9e0939022376d59756609fa3153
FukeKazki <kazkichi0906@gmail.com>
2019-09-10 23:39:43 +0900

    Fix: error処理

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
-import express from 'express';
-import * as line from '@line/bot-sdk';
-import crypto from 'crypto';
-import axios from 'axios';
-import { config }  from './setting';
+import express from 'express'
+import * as line from '@line/bot-sdk'
+import crypto from 'crypto'
+import axios from 'axios'
+import {
+    config
+} from './setting'
 
-const app = express();
-const client = new line.Client(config);
-const PORT = process.env.PORT || 3000;
+const app = express()
+const client = new line.Client(config)
+const PORT = process.env.PORT || 3000
 
-app.post('/webhook', line.middleware(config), (req, res) => {
-    if (!validate_signature(req.headers['x-line-signature'], req.body)) return;
-    const replyToken = req.body.events[0].replyToken;
-    const messageText = req.body.events[0].message.text;
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
 
-    searchQiitaArticle(messageText).then((data) => {
-        replyText(replyToken, data);
-    }).catch((e) => {
-        console.log('REJECT_ERROR' + e);
-        replyText(replyToken, 'エラーが発生しました.');
-    });
-
-});
-app.get('/', (req, res) => {
-    res.json({text: 'Hello World!'});
-});
-app.listen(PORT);
-console.log(`Server running at ${PORT}`);
-
+app.post('/webhook', line.middleware(config), async (req, res) => {
+    if (!validate_signature(req.headers['x-line-signature'], req.body)) return
+    const replyToken = req.body.events[0].replyToken
+    const messageText = req.body.events[0].message.text
+    const articles = await searchQiitaArticle(messageText).catch(e => 'NOT FOUND')
+    replyText(replyToken, articles)
+})
+app.listen(PORT)
+console.log(`Server running at ${PORT}`)
 
 /**
  * 署名の検証
@@ -38,26 +36,26 @@ const validate_signature = (signature, body) => {
     return signature === crypto.createHmac('SHA256', config.channelSecret).update(JSON.stringify(body)).digest('base64');
 };
 
-const searchQiitaArticle = async(tag) => {
-    const url = `http://qiita.com/api/v2/items?page=1&per_page=5&query=tag:${encodeURIComponent(tag)}`;
-    try {
-       const articles = await axios.get(url);
-       let titles = '';
-       for(const item of articles.data) {
-          titles += item.title + '\n' + item.url + '\n\n';
-       }
-       return Promise.resolve(titles);
-    } catch (e) {
-       console.log('rejectします');
-       return Promise.reject(e);
-    }
-};
-
+const searchQiitaArticle = async (tag) => {
+    const url = `http://qiita.com/api/v2/items?page=1&per_page=5&query=tag:${encodeURIComponent(tag)}`
+    const articles = await axios.get(url).catch(e => Promise.reject('Error'))
+    return new Promise(async (resolve, reject) => {
+        if (articles.data.length === 0) reject('NOT FOUND')
+        let titles = ''
+        for (const item of articles.data) {
+            titles += item.title + '\n' + item.url + '\n\n';
+        }
+        resolve(titles)
+    })
+}
 /**
  * 返信する
  * @param replyToken
  * @param text
  */
 const replyText = (replyToken, text = 'DEFAULT_TEXT') => {
-    client.replyMessage(replyToken, {type: 'text', text: text});
+    client.replyMessage(replyToken, {
+        type: 'text',
+        text: text
+    });
 };
